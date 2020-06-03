df1d9e72441d9e6b35c1b0daf2b71a9f69829a77
Damian Michalak <michalak.damian.kul@gmail.com>
2020-06-03 20:02:50 +0200

    Initial commit

---
+++ src/main/java/com/kuljava/swiatwsi/SwiatWsiSerwerApplication.java
@@ -0,0 +1,13 @@
+package com.kuljava.swiatwsi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SwiatWsiSerwerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SwiatWsiSerwerApplication.class, args);
+    }
+
+}

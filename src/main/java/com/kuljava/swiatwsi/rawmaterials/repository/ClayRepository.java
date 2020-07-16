761b846e5f13bb8af3b3e32597b9094fe4fec9aa
PawelPaziewski <lrpapawel@gmail.com>
2020-07-16 17:23:06 +0200

    Add clay entity and repo

--- src/main/java/com/kuljava/swiatwsi/rawmaterials/repository/ClayRepository.java
+++ src/main/java/com/kuljava/swiatwsi/rawmaterials/repository/ClayRepository.java
@@ -2,6 +2,8 @@ package com.kuljava.swiatwsi.rawmaterials.repository;
 
 import com.kuljava.swiatwsi.rawmaterials.entity.Clay;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface ClayRepository extends JpaRepository<Clay, Long> {
 }

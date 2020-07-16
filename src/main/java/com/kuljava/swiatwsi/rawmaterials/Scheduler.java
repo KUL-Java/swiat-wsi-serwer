321693647b0a3ee65f6318942f608c0e77b79fc9
PawelPaziewski <lrpapawel@gmail.com>
2020-07-16 18:33:38 +0200

    Make Scheduler increase quantity of raw materials

--- src/main/java/com/kuljava/swiatwsi/rawmaterials/Scheduler.java
+++ src/main/java/com/kuljava/swiatwsi/rawmaterials/Scheduler.java
@@ -1,22 +1,28 @@
 package com.kuljava.swiatwsi.rawmaterials;
 
-import org.dom4j.CDATA;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import com.kuljava.swiatwsi.rawmaterials.repository.ClayRepository;
+import com.kuljava.swiatwsi.rawmaterials.repository.IronRepository;
+import com.kuljava.swiatwsi.rawmaterials.repository.WoodRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
-import java.text.SimpleDateFormat;
-import java.util.Date;
-
 @Component
 public class Scheduler {
-    private static final Logger log = LoggerFactory.getLogger(Scheduler.class);
-
-    private static final SimpleDateFormat dateFormat = new SimpleDateFormat("HH:mm:ss");
+    private static final Long WOODINCREASE = 10L;
+    private static final Long CLAYINCREASE = 10L;
+    private static final Long IRONINCREASE = 10L;
+    @Autowired
+    WoodRepository wood;
+    @Autowired
+    ClayRepository clay;
+    @Autowired
+    IronRepository iron;
 
     @Scheduled(fixedRate = 1000)
-    public void reportCurrentTime() {
-        log.info("The time is now {}", dateFormat.format(new Date()));
+    public void increaseQuantities() {
+        wood.increaseAll(WOODINCREASE);
+        clay.increaseAll(CLAYINCREASE);
+        iron.increaseAll(IRONINCREASE);
     }
 }

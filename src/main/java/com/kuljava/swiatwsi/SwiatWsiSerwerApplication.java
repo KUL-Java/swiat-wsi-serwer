e00649d3fbdd74d1ef034288700c5b7422a8e40a
GroteJongen <50738377+GroteJongen@users.noreply.github.com>
2020-07-18 13:15:02 +0200

    Removed redundant bean

--- src/main/java/com/kuljava/swiatwsi/SwiatWsiSerwerApplication.java
+++ src/main/java/com/kuljava/swiatwsi/SwiatWsiSerwerApplication.java
@@ -1,28 +1,12 @@
 package com.kuljava.swiatwsi;
 
-import com.kuljava.swiatwsi.exceptions.VillagesAmountExceededException;
-import com.kuljava.swiatwsi.services.VillageService;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class SwiatWsiSerwerApplication {
 
   public static void main(String[] args) {
     SpringApplication.run(SwiatWsiSerwerApplication.class, args);
-  }
-  @Bean
-  CommandLineRunner runner(VillageService villageService) {
-    return (args) -> {
-      for (int i = 1; i < 999; i++) {
-        try {
-          villageService.saveVillage("Niedrzwica " + i);
-        } catch (VillagesAmountExceededException e) {
-          System.out.println("Niestety, " + e.getMessage());
-        }
-      }
-    };
   }
 }

308c437c380f79280f7b6cd47db35c37a0b53a50
GroteJongen <50738377+GroteJongen@users.noreply.github.com>
2020-07-18 13:13:00 +0200

    Code refactor after review - added response status in VillageController

--- src/main/java/com/kuljava/swiatwsi/world/VillageController.java
+++ src/main/java/com/kuljava/swiatwsi/world/VillageController.java
@@ -4,6 +4,7 @@ import com.kuljava.swiatwsi.exceptions.VillagesAmountExceededException;
 import com.kuljava.swiatwsi.services.VillageService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.server.ResponseStatusException;
 
@@ -27,12 +28,15 @@ public class VillageController {
   }
 
   @PostMapping()
-  public Optional<Village> addVillage(@RequestParam String name) {
+  public ResponseEntity<Village> addVillage(@RequestParam String name) {
     try {
-      return villageService.saveVillage(name);
+      final Optional<Village> village = villageService.saveVillage(name);
+      return village
+          .map(value -> new ResponseEntity<>(value, HttpStatus.CREATED))
+          .orElseGet(() -> new ResponseEntity<>(HttpStatus.CONFLICT));
     } catch (VillagesAmountExceededException e) {
       throw new ResponseStatusException(
-          HttpStatus.CONFLICT, "The server is full, cannot create another village", e);
+          HttpStatus.SERVICE_UNAVAILABLE, "The server is full, cannot create another village", e);
     }
   }
 }

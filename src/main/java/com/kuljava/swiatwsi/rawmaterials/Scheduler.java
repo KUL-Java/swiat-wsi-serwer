8debcc20679d6ec8620a1c24545a453386e9045a
PawelPaziewski <lrpapawel@gmail.com>
2020-09-01 18:50:18 +0200

    Surowce - make amount of increase set in application.properties

--- src/main/java/com/kuljava/swiatwsi/rawmaterials/Scheduler.java
+++ src/main/java/com/kuljava/swiatwsi/rawmaterials/Scheduler.java
@@ -1,27 +1,30 @@
 package com.kuljava.swiatwsi.rawmaterials;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
 @Component
 public class Scheduler {
-    private Long woodIncrease;
-    private Long clayIncrease;
-    private Long ironIncrease;
 
-    public Scheduler() {
-        woodIncrease = 10l;
-        clayIncrease = 10l;
-        ironIncrease = 10l;
-    }
+  @Autowired RawMaterialsRepository repository;
 
+  private Long woodIncrease;
+  private Long clayIncrease;
+  private Long ironIncrease;
 
-    @Autowired
-    RawMaterialsRepository repository;
+  public Scheduler(
+      @Value("${rawmaterials.scheduler.woodincrease}") String woodIncrease,
+      @Value("${rawmaterials.scheduler.clayincrease}") String clayIncrease,
+      @Value("${rawmaterials.scheduler.ironincrease}") String ironIncrease) {
+    this.woodIncrease = Long.parseLong(woodIncrease);
+    this.clayIncrease = Long.parseLong(clayIncrease);
+    this.ironIncrease = Long.parseLong(ironIncrease);
+  }
 
-    @Scheduled(fixedRateString = "${rawmaterials.scheduler.fixedRate}")
-    public void increaseQuantities() {
-        repository.increaseAll(woodIncrease, clayIncrease, ironIncrease);
-    }
+  @Scheduled(fixedRateString = "${rawmaterials.scheduler.fixedRate}")
+  public void increaseQuantities() {
+    repository.increaseAll(woodIncrease, clayIncrease, ironIncrease);
+  }
 }
